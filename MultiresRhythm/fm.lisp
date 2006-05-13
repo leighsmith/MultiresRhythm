5a49c11059699ca9282c4c452b98defbd37ebdfc
leigh <leigh@fe1d2b8c-0a59-44fb-bae1-36f596c3a4c4>
2006-05-13 13:08:51 +0000

    Made signal length a keyword parameter
    git-svn-id: file:///Users/leigh/Sources/SVN/Research_repos/Research@228 fe1d2b8c-0a59-44fb-bae1-36f596c3a4c4

--- MultiresRhythm/fm.lisp
+++ MultiresRhythm/fm.lisp
@@ -1,13 +1,13 @@
-(defun fm-test ()
+(defun fm-test (&key (signal-length 2048))
   "Frequency modulating signal for wavelet analysis."
-  (let* ((signalLength 2048)
-	 (carrierFreq 16)
+  (let* ((carrierFreq 16)
 	 (modFreq 2.1)
-	 (norm-signal (.rseq 0 1 signalLength))
+	 (norm-signal (.rseq 0 1 signal-length))
 	 (mod (.* 3.0 (.cos (.* norm-signal 2.0 pi modFreq))))
 	 (fm-signal (.cos (.+ (.* 2 pi carrierFreq norm-signal) mod))))
     ;; (plot mod nil)
     (plot fm-signal nil)
     fm-signal))
 
-;; (dyadic-cwt (fm-test) 8 512)
+;; (setf x (dyadic-cwt (fm-test) 8 512))
+;; (setf x (dyadic-cwt (fm-test :signal-length 256) 8 256))
