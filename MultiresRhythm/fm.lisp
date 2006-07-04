a88deb0a6ee318fda15238fc99ea5870c5456ad8
leigh <leigh@fe1d2b8c-0a59-44fb-bae1-36f596c3a4c4>
2006-07-04 22:00:29 +0000

    Added rising harmonic example
    git-svn-id: file:///Users/leigh/Sources/SVN/Research_repos/Research@252 fe1d2b8c-0a59-44fb-bae1-36f596c3a4c4

--- MultiresRhythm/fm.lisp
+++ MultiresRhythm/fm.lisp
@@ -1,3 +1,24 @@
+;;;; -*- Lisp -*-
+;;;;
+;;;; $Id$
+;;;;
+;;;; Functions for testing using various signals.
+;;;;
+;;;; In nlisp (Matlab-alike Common Lisp library www.nlisp.info)
+;;;;
+;;;; By Leigh M. Smith <lsmith@science.uva.nl> 
+;;;;
+;;;; Copyright (c) 2006
+;;;;
+;;;; See 
+;;;;   author =  {Leigh M. Smith},
+;;;;   title =   {A Multiresolution Time-Frequency Analysis and Interpretation of Musical Rhythm},
+;;;;   school =  {Department of Computer Science, University of Western Australia},
+;;;;   year =    1999,
+;;;;   month =   {June},
+;;;;   annote =  {\url{http://www.leighsmith.com/Research/Papers/MultiresRhythm.pdf}}
+;;;;
+
 (defun fm-test (&key (signal-length 2048))
   "Frequency modulating signal for wavelet analysis."
   (let* ((carrierFreq 16)
@@ -10,5 +31,23 @@
     fm-signal))
 
 ;; (multiple-value-setq (fm-mag fm-phase) (dyadic-cwt (fm-test) 8 512))
-;; (plot-cwt (dyadic-cwt (fm-test) 8 512))
+;; (multiple-value-setq (fm-mag fm-phase) (cwt (fm-test) 8))
+;; (plot-cwt (cwt (fm-test) 8))
 ;; (setf x (dyadic-cwt (fm-test :signal-length 256) 8 256))
+;; (multiple-value-bind (fm-mag fm-phase) (cwt (fm-test) 8) (plot-cwt fm-mag fm-phase))
+
+;;; 
+(defun rising-harmonic-test (&key (signal-length 2048))
+  "Analyse a harmonic (ie. cosinusoidal with harmonics) signal undergoing frequency change"
+  ;; a nice set of JI ratios 1/1 + octave+8/5 + 2 octaves+6/5
+  (let* ((norm-signal (.rseq 0 1 signal-length))
+	 (periodicity (.+ (.* norm-signal 16) 4))
+	 (periodicity2 (.* periodicity (+ 1 8/5)))
+	 (periodicity3 (.* periodicity (+ 4 6/5)))
+	 (rising-harmonic-signal (.+ (.* 0.33 (.cos (.* norm-signal 2.0 pi periodicity)))
+				     (.* 0.33 (.cos (.* norm-signal 2.0 pi periodicity2)))
+				     (.* 0.33 (.cos (.* norm-signal 2.0 pi periodicity3))))))
+    (plot rising-harmonic-signal nil)
+    rising-harmonic-signal))
+	 
+;; (multiple-value-bind (rising-harmonic-mag rising-harmonic-phase) (cwt (rising-harmonic-test) 8) (plot-cwt rising-harmonic-mag rising-harmonic-phase :title "rising-harmonic"))
