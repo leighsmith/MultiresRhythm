9442e0651265b44f1cecd41b4d0e00b33c324cd1
leigh <leigh@fe1d2b8c-0a59-44fb-bae1-36f596c3a4c4>
2006-09-30 13:22:27 +0000

    Added extra tests, corrected declarations
    git-svn-id: file:///Users/leigh/Sources/SVN/Research_repos/Research@357 fe1d2b8c-0a59-44fb-bae1-36f596c3a4c4

--- Syncopation/trunk/Handel.lisp
+++ Syncopation/trunk/Handel.lisp
@@ -1,9 +1,11 @@
-;;; $Id: Shmulevitch.lisp 4703 2006-02-09 23:04:31Z leigh $
+;;; $Id$
 ;;;
 ;;; Taken from Handel '93
 ;;; First 13 patterns of table 1.
 ;;; Each 1 represents a tone, 0 represents a rest, the smallest IOI is 200ms.
 ;;;
+(in-package :syncopation)
+
 (defparameter *handel-patterns*
   '((1 0 0 1 0 0 1 0 1 0 0 1 0 0 0 0)
     (1 0 0 1 0 0 1 0 0 1 0 1 0 0 0 0)
@@ -17,13 +19,18 @@
     (1 0 0 1 0 1 0 0 0 1 0 1 0 0 0 0)
     (1 0 1 0 0 1 0 0 1 0 0 1 0 0 0 0)
     (1 0 1 0 1 0 0 0 1 0 0 1 0 0 0 0)
-    (1 0 0 1 0 0 0 1 0 1 0 1 0 0 0 0))
+    (1 0 0 1 0 0 0 1 0 1 0 1 0 0 0 0)))
 
 ;;; TODO 
 ;;; Reduce each pattern to 12 beats in order to test against triplet meters. Drop the last
 ;;; 4 beats but then re-instate the terminating beat, leaving 13 beats.
 
 ;; Handel's non-metrical example on p372
-;; (test-tactus-for-rhythm "handel-nonmetrical-1" '(1 0 1 0 0 0 1 0 0 1 0 1 0 0 0 0))
+;; (multires-rhythm::test-tactus-for-rhythm "handel-nonmetrical-1" '(1 0 1 0 0 0 1 0 0 1 0 1 0 0 0 0))
 ;;vs. 
-;; (test-tactus-for-rhythm "handel-nonmetrical-2" '(1 0 1 0 0 1 0 0 0 1 0 1 0 0 0 0))
+;; (multires-rhythm::test-tactus-for-rhythm "handel-nonmetrical-2" '(1 0 1 0 0 1 0 0 0 1 0 1 0 0 0 0))
+
+;; (multires-rhythm::test-tactus-for-rhythm "handel-pattern-1" (repeat-rhythm (nth 0 *handel-patterns*) 2))
+
+;; Must do two repetitions.
+; (multires-pattern-complexity "shmulevich" *handel-patterns*)
