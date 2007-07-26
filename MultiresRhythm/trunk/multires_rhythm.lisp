d075d983dc27d3febcb410b4b1acf2479c60a0f5
leigh <leigh@fe1d2b8c-0a59-44fb-bae1-36f596c3a4c4>
2007-07-26 10:17:50 +0000

    Factored clapping production into it's own file, made the preferred tempo function no longer depend on a scaleogram (VPO is sufficient), renaming it preferred-tempo-scale.
    git-svn-id: file:///Users/leigh/Sources/SVN/Research_repos/Research@510 fe1d2b8c-0a59-44fb-bae1-36f596c3a4c4

--- MultiresRhythm/trunk/multires_rhythm.lisp
+++ MultiresRhythm/trunk/multires_rhythm.lisp
@@ -22,9 +22,6 @@
 ;;;; correlated-ridge-scale-peaks frames?
 ;;;; Declarations (see rhythm.lisp for the class definition)
 
-(defgeneric clap-to-rhythm (rhythm-to-analyse &key tactus-selector start-from-beat)
-  (:documentation "Returns a set of sample times to clap to given the supplied rhythm"))
-
 (defgeneric tactus-for-rhythm (rhythm-to-analyse &key voices-per-octave tactus-selector)
   (:documentation "Returns the selected tactus for the rhythm."))
 
@@ -50,16 +47,16 @@ otherwise, generates them and writes to disk."))
 ;; Slowest scale "accessible to the senses" (London 2004)
 ;; (scale-from-period (* 200 5.0) 16)
 
-(defun preferred-tempo (rhythm-scaleogram rhythm-sample-rate 
-			;; Fraisse's "spontaneous tempo" interval in seconds
-			&key (tempo-salience-peak 0.600))
+(defun preferred-tempo-scale (voices-per-octave rhythm-sample-rate 
+			      ;; Fraisse's "spontaneous tempo" interval in seconds
+			      &key (tempo-salience-peak 0.600))
   "Determine the scale which Fraisse's spontaneous tempo would occur at.
 This is weighted by absolute constraints, look in the 600ms period range."
   (let* ((salient-IOI (* rhythm-sample-rate tempo-salience-peak)))
     ;; Convert salient-IOI into the scale to begin checking.
     ;; This assumes the highest frequency scale (shortest time support)
     ;; is index 0, lowest frequency (longest time support) is number-of-scales.
-    (floor (scale-from-period salient-IOI (voices-per-octave rhythm-scaleogram)))))
+    (floor (scale-from-period salient-IOI voices-per-octave))))
 
 ;; Scale index 0 is the highest frequency (smallest dilation) scale.
 (defun tempo-salience-weighting (salient-scale time-frequency-dimensions)
@@ -311,7 +308,7 @@ and stationary phase measures, optionally weighed by absolute tempo preferences.
 	 ;; Correlate various ridges to produce a robust version.
 	 (correlated-ridges (correlate-ridges magnitude phase vpo))
 	 ;; Scale index 1 is the highest frequency (smallest dilation) scale.
-	 (salient-scale (preferred-tempo scaleogram sample-rate))
+	 (salient-scale (preferred-tempo-scale (voices-per-octave scaleogram) sample-rate))
 	 (tempo-weighting (tempo-salience-weighting salient-scale (.array-dimensions magnitude)))
 	 ;; Weight by the absolute tempo preference.
 	 (tempo-weighted-ridges (.* correlated-ridges tempo-weighting)))
@@ -382,73 +379,6 @@ and stationary phase measures, optionally weighed by absolute tempo preferences.
 (defun clean-phase (magnitude phase &key (threshold 0.001) (clamp 0.0))
   "We clamp any ill-conditioned phase (negligble magnitude) to the value given (defaulting to zero)"
   (clamp-to-bounds phase magnitude :low-bound threshold :clamp-low clamp))
-
-;; TODO Return ((time intensity) (time intensity) ...)
-;; use constant intensity claps but weight the amplitude for when we mix in Common Music.
-(defun clap-to-tactus-phase (original-rhythm rhythm-scaleogram tactus 
-			     &key (start-from-beat 0))
-  "Function to compute times to clap and how hard from the extracted tactus.
-   Returns a matrix where row 1 is the time to tap at, row 2 is the intensity."
-  (let* ((time-frequency-dimensions (.array-dimensions (scaleogram-magnitude rhythm-scaleogram)))
-	 (time-in-samples (second time-frequency-dimensions))
-
-	 ;; The right way to do this is with a tight (well, two octave)
-	 ;; Gaussian envelope over it.
-	 ;; tactus-mag (gaussian-envelope tactus voices-per-octave)
-	 ;; But just a single voice alone produces the correct oscillation, with lower amplitude:
-	 (empty-magnitude (make-double-array time-frequency-dimensions))
-	 (tactus-mag (dolist (tactus-ridge (if (listp tactus) tactus (list tactus)) empty-magnitude)
-			     (insert-ridge tactus-ridge empty-magnitude :constant-value 1d0)))
-
-	 ;; TODO To be completely correct the original padded output from dyadic-cwt should be used
-	 ;; for input to the dyadic-icwt.
-	 ;; TODO create a scalogram
-	 ;; (clamped-magnitude-scaleogram (copy-instance rhythm-scaleogram))
-	 ;; (setf (scaleogram-magnitude clamped-magnitude-scalogram) tactus-mag)
-	 ;; (icwt clamped-magnitude-scaleogram)
-	 ;; Use the modified magnitude and original phase to compute a sinusoid and it's
-	 ;; Hilbert transform, from that, determine the phase of the sinusoid.
-	 (foot-tap-phase (.phase (icwt tactus-mag
-				       (scaleogram-phase rhythm-scaleogram)
-				       (voices-per-octave rhythm-scaleogram))))
-
-	 ;; Note the phase of the oscillating sinusoid at the beat to start tapping from.
-	 (down-beat-sample (time-of-beat original-rhythm start-from-beat))
-	 (clap-on-phase-datum (.aref foot-tap-phase down-beat-sample))
-
-	 ;; check clap-on-phase-datum >= current and < next phase measure.
-	 ;; TODO this could be a problem on the last phase point before 2 pi wrap.
-	 ;; identify reoccurance of the initial clap phase across the translation (time) axis
-	 (phase-reoccured (.and (.>= clap-on-phase-datum 
-				     (.subarray foot-tap-phase (list 0 (list 0 (- time-in-samples 2))))) 
-				(.< clap-on-phase-datum 
-				    (.subarray foot-tap-phase (list 0 (list 1 (1- time-in-samples)))))))
-	 (clap-at (.find phase-reoccured))
-	 ;; Create a clapping rhythm
-	 ;; (clap-rhythm (rhythm-of-onsets (name original-rhythm) clap-at))
-	 )
-
-    (format t "Handclapping from beat ~d of original rhythm, sample ~d~%" start-from-beat down-beat-sample)
-    (plot-claps original-rhythm clap-at :foot-tap-AM foot-tap-phase)
-    clap-at))
-
-(defmethod clap-to-rhythm ((performed-rhythm rhythm) &key 
-			   (start-from-beat 1)
-			   (tactus-selector #'select-longest-lowest-tactus))
-  "Returns a set of sample times to clap to given the supplied rhythm"
-  (multiple-value-bind (computed-tactus rhythm-scaleogram)
-      (tactus-for-rhythm performed-rhythm :tactus-selector tactus-selector)
-    (clap-to-tactus-phase performed-rhythm rhythm-scaleogram computed-tactus :start-from-beat start-from-beat)))
-
-(defun save-rhythm-and-claps (original-rhythm clap-at)
-  "Writes out the rhythm and the handclaps to a scorefile"
-  (save-scorefile (format nil "/Users/leigh/~a.handclap.score" (name original-rhythm)) 
-		  (list (nlisp::array-to-list (onsets-in-seconds original-rhythm)) 
-			(nlisp::array-to-list (./ clap-at (* (sample-rate original-rhythm) 1d0))))
-		  :instrument "midi"
-		  :midi-channel 10
-		  :key-numbers (list *low-woodblock* *closed-hi-hat*)
-		  :description (format nil "Handclapping to ~a" (description original-rhythm))))
 
 ;;; File I/O
 
