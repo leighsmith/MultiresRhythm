05f891740895f78fb1c8605b7f1659b04250089d
leigh <leigh@fe1d2b8c-0a59-44fb-bae1-36f596c3a4c4>
2007-09-21 11:45:05 +0000

    Added subdirectories to allow better management of files.
    git-svn-id: file:///Users/leigh/Sources/SVN/Research_repos/Research@543 fe1d2b8c-0a59-44fb-bae1-36f596c3a4c4

--- MultiresRhythm/branches/Plymouth-Perceptual-Onsets/NIPS_examples.lisp
+++ MultiresRhythm/branches/Plymouth-Perceptual-Onsets/NIPS_examples.lisp
@@ -1,3 +1,8 @@
+; -*- Lisp -*-
+
+(in-package :multires-rhythm)
+(use-package :nlisp)
+
 ;;; Drums
 (compute-perceptual-versions "res3_1_resp_text"  "res3_1_pOnsets_text"  "res3_1")
 (compute-perceptual-versions "res3_2_resp_text"  "res3_2_pOnsets_text"  "res3_2")
@@ -7,11 +12,20 @@
 (compute-perceptual-versions "res3_20_resp_text" "res3_20_pOnsets_text" "res3_20")
 (compute-perceptual-versions "res3_38_resp_text" "res3_38_pOnsets_text" "res3_38")
 
+;;; Sung Saxophone lines
+(compute-perceptual-versions "res2/res2_20_resp_text"  "res2/res2_20_pOnsets_text"  "res2/res2_20")
+
 ;;; Freely Sung examples.
-(compute-perceptual-versions "res1_1_resp_text" "res1_1_pOnsets_text" "res1_1")
-;; Clapping from 0 creates a phase problem with res1_2
-(compute-perceptual-versions "res1_2_resp_text" "res1_2_pOnsets_text" "res1_2" :start-from-beat 1) 
-(compute-perceptual-versions "res1_3_resp_text" "res1_3_pOnsets_text" "res1_3")
-(compute-perceptual-versions "res4_1_resp_text" "res4_1_pOnsets_text" "res4_1")
-(compute-perceptual-versions "res4_2_resp_text" "res4_2_pOnsets_text" "res4_2")
-(compute-perceptual-versions "res4_3_resp_text" "res4_3_pOnsets_text" "res4_3")
+(compute-perceptual-versions "res1/res1_1_resp_text" "res1/res1_1_pOnsets_text" "res1/res1_1")
+;; There is a glitch at the beginning of the recording so that clapping from 0 creates a phase problem with res1_2
+;; So for now we use a start from 1st beat. 
+(compute-perceptual-versions "res1/res1_2_resp_text" "res1/res1_2_pOnsets_text" "res1/res1_2" :start-from-beat 1)
+(compute-perceptual-versions "res1/res1_3_resp_text" "res1/res1_3_pOnsets_text" "res1/res1_3")
+(compute-perceptual-versions "res4/res4_1_resp_text" "res4/res4_1_pOnsets_text" "res4/res4_1")
+(compute-perceptual-versions "res4/res4_2_resp_text" "res4/res4_2_pOnsets_text" "res4/res4_2")
+;; The ridge selected by the weighted version is too slow, the unweighted version
+;; matches the beat rate.
+(compute-perceptual-versions "res4/res4_3_resp_text" "res4/res4_3_pOnsets_text" "res4/res4_3")
+;; Doing this with a beat-multiple of 3 correctly claps.
+(compute-perceptual-versions "res4/res4_3_resp_text" "res4/res4_3_pOnsets_text" "res4/res4_3" :start-from-beat 5)
+
