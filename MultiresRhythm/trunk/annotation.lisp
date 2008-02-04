6bb5949ab5819497bd644d0101e7d68433fbf106
leigh <leigh@fe1d2b8c-0a59-44fb-bae1-36f596c3a4c4>
2008-02-04 14:58:26 +0000

    Added draft extension of the annotation routine as commented out code.
    git-svn-id: file:///Users/leigh/Sources/SVN/Research_repos/Research@626 fe1d2b8c-0a59-44fb-bae1-36f596c3a4c4

--- MultiresRhythm/trunk/annotation.lisp
+++ MultiresRhythm/trunk/annotation.lisp
@@ -23,13 +23,53 @@
        for ridges-for-note = (ridges-at-time rhythm-skeleton note-time)
        collect (list note-time (ridge-ratios-at-time ridges-for-note tactus note-time)))))
 
-(defmethod annotate-rhythm ((rhythm-to-annotate rhythm) (rhythm-skeleton skeleton) (tactus ridge))
+(defmethod annotate-rhythm ((rhythm-to-annotate rhythm) (rhythm-skeleton skeleton) (tactus ridge)
+			     &key (start-from-beat 0))
   "Given the rhythm and the skeleton of ridges, produce an annotation of each note with
   respect to the tactus."
   ;; We could use Desain & Honing categorisation data to unify or prune the ratio
   ;; lists. This should be implemented as a separate function, of course.
   ;; Need also an underlying measure of ratio uncertainty due the voices-per-octave resolution.
   (ratios-to-tactus rhythm-to-annotate rhythm-skeleton tactus))
+
+#|
+(defmethod annotate-rhythm ((rhythm-to-annotate rhythm) (rhythm-skeleton skeleton) (tactus ridge)
+			    &key (start-from-beat 0))
+  "Given the rhythm and the skeleton of ridges, produce an annotation of each note with
+  respect to the tactus."
+  ;; We could use Desain & Honing categorisation data to unify or prune the ratio
+  ;; lists. This should be implemented as a separate function, of course.
+  ;; Need also an underlying measure of ratio uncertainty due the voices-per-octave resolution.
+  (let* ((time-frequency-dimensions (.array-dimensions (scaleogram-magnitude rhythm-scaleogram)))
+	 (time-in-samples (second time-frequency-dimensions))
+	 
+	 ;; The right way to do this is with a tight (well, two octave)
+	 ;; Gaussian envelope over it.
+	 ;; tactus-mag (gaussian-envelope tactus voices-per-octave)
+	 ;; But just a single voice alone produces the correct oscillation, with lower amplitude:
+	 (empty-magnitude (make-double-array time-frequency-dimensions))
+	 (tactus-mag (dolist (tactus-ridge (if (listp tactus) tactus (list tactus)) empty-magnitude)
+			     (insert-ridge tactus-ridge empty-magnitude :constant-value 1d0)))
+
+	 ;; TODO To be completely correct the original padded output from dyadic-cwt should be used
+	 ;; for input to the dyadic-icwt.
+	 ;; TODO create a scalogram
+	 ;; (clamped-magnitude-scaleogram (copy-instance rhythm-scaleogram))
+	 ;; (setf (scaleogram-magnitude clamped-magnitude-scalogram) tactus-mag)
+	 ;; (icwt clamped-magnitude-scaleogram)
+	 ;; Use the modified magnitude and original phase to compute a sinusoid and it's
+	 ;; Hilbert transform, from that, determine the phase of the sinusoid.
+	 (foot-tap-phase (.phase (icwt tactus-mag
+				       (scaleogram-phase rhythm-scaleogram)
+				       (voices-per-octave rhythm-scaleogram))))
+
+	 ;; Note the phase of the oscillating sinusoid at the beat to start tapping from.
+	 (downbeat-sample (onset-time-of-beat rhythm-to-annotate start-from-beat))
+	 (downbeat-phase-datum (.aref foot-tap-phase downbeat-sample)))
+(./ (.- (.arefs foot-tap-phase (onsets-in-samples rhythm-to-annotate)) downbeat-phase-datum)
+    (* 2 pi))
+    ))
+|#
 
 (defmethod annotate-rhythm ((rhythm-to-annotate rhythm))
   "Given the rhythm, produce an annotation of each note with respect to the tactus"
