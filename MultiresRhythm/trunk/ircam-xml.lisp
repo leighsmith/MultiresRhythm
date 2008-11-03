d44d19cbffedf338f6bd8d4b7843816ee41bd713
leigh <leigh@fe1d2b8c-0a59-44fb-bae1-36f596c3a4c4>
2008-11-03 14:02:06 +0000

    Added XML marker file writing routine, using SAX handler. Ideally we can build a DOM object first
    git-svn-id: file:///Users/leigh/Sources/SVN/Research_repos/Research@754 fe1d2b8c-0a59-44fb-bae1-36f596c3a4c4

--- MultiresRhythm/trunk/ircam-xml.lisp
+++ MultiresRhythm/trunk/ircam-xml.lisp
@@ -1,10 +1,25 @@
 ;;;; -*- Lisp -*-
+;;;;
+;;;; $Id$
+;;;;
 ;;;; Routines to read (and one day write) ircam-beat XML files.
 ;;;; Uses the CXML package and the accompanying DOM package.
+;;;;
+;;;; In nlisp (Matlab-alike Common Lisp library www.nlisp.info)
+;;;;
+;;;; By Leigh M. Smith <Leigh.Smith@ircam.fr> 
+;;;;
+;;;; Copyright (c) 2008
+;;;;
+
+(require 'cxml)
+
+(in-package :multires-rhythm)
+(use-package :nlisp)
 
 #|
 (setf bpm-doc (cxml:parse
-	       #P"/Local/Users/leigh/Research/Sources/Rhythm/IRCAM/LeighsTests/res4_1.wav.bpm.xml"
+	       #P"/Local/Users/leigh/Research/Data/IRCAM-Beat/res4_1.wav.bpm.xml"
 	       (cxml-dom:make-dom-builder)))
 (dom:document-element bpm-doc)
 (dom:tag-name (dom:document-element bpm-doc))
@@ -24,6 +39,9 @@
 (dom:item a 0)
 (dom:child-nodes (dom:item a 0))
 (dom:node-value (dom:item (dom:child-nodes (dom:item a 0)) 0))
+
+;;; Creation
+(dom:create-document (dom:implementation bpm-doc) nil nil nil)
 |#
 
 (defun read-ircam-beat-markers (filepath)
@@ -37,21 +55,51 @@
        collect (read-from-string (dom:node-value (dom:item (dom:child-nodes marker-node) 0))) into marker-list
        finally (return (make-narray marker-list)))))
 
-
-;; (setf ircam-beat-markers (read-ircam-beat-markers #P"/Local/Users/leigh/Research/Sources/Rhythm/IRCAM/LeighsTests/res4_1.wav.markers.xml"))
-;; (setf mrr-markers (make-narray '(0.1d0 0.69d0 1.26d0 1.8d0 2.325d0 2.865d0 3.445d0 4.155d0 4.855d0  5.52d0 6.165d0 6.795d0 7.425d0 8.025d0 8.58d0 9.125d0)))
-;; (nplot (list (.diff ircam-beat-markers) (.diff mrr-markers)) nil :styles '("linespoints" "linespoints") :aspect-ratio 0.66)
-
-;; (setf res4_2-ircam-beat-markers (read-ircam-beat-markers #P"/Local/Users/leigh/Research/Sources/Rhythm/IRCAM/LeighsTests/res4_2.wav.markers.xml"))
-;; (setf res4_2-mrr-markers (make-narray '(0.475d0 0.985d0 1.54d0 2.2d0 2.85d0 3.495d0 4.145d0 4.825d0 5.56d0
-;;         6.285d0 6.985d0 7.695d0 8.38d0 9.04d0 9.69d0 10.325d0 10.93d0 11.51d0)))
-;; (nplot (list (.diff res4_2-ircam-beat-markers) (.diff res4_2-mrr-markers)) nil :styles '("linespoints" "linespoints") :aspect-ratio 0.66)
-
 ;;;; To serialize a DOM document, use a SAX serialization sink as the argument to dom:map-document, which generates SAX events for the DOM tree
-;;; Create a "beatdescription", holding:
-;;; "media" node
-;;; Multiple "segments"
-;;; "markerDS" holding:
-;;; "marker"
-;;; Have to fake out the metrical descriptors.
-;;; (defun write-ircam-beat-marker (media-description clap-times-in-seconds)
+;;; Will have to fake out the metrical descriptors.
+(defun create-ircam-beat-marker-document (media-description clap-times-in-seconds)
+  (let* ((document (dom:create-document (dom:implementation bpm-doc) nil nil nil))
+	 (beat-description (dom:create-element document "beatdescription"))
+	 (media (dom:create-element document "media"))
+	 (marker-DS (dom:create-element document "markerDS")))
+    (loop
+       for time across (val clap-times-in-seconds)
+       do (dom:create-element document "marker"))
+    (dom:create-attribute document )
+    document))
+
+(defun write-ircam-beat-marker-stream (stream document)
+  (dom:map-document (cxml:make-octet-stream-sink stream :indentation 2 :canonical nil)
+		    document))
+
+(defun write-ircam-beat-marker-document (filepath document)
+  (with-open-file (out filepath :direction :output :element-type '(unsigned-byte 8))
+    (write-ircam-beat-marker-stream out document)))
+
+(defun write-ircam-beat-marker-document (filepath media-description clap-times-in-seconds)
+  (write-ircam-beat-marker-document filepath 
+				    (create-ircam-beat-marker-document media-description clap-times-in-seconds)))
+
+(defun write-ircam-beat-marker-stream (stream media-description clap-times-in-seconds meter last-time)
+  (cxml:with-xml-output (cxml:make-octet-stream-sink stream :indentation 2 :canonical nil)
+    (cxml:with-element "beatdescription"
+      (cxml:with-element "media"
+	(cxml:text media-description))
+      (cxml:with-element "segment"
+	(cxml:attribute "start" "0.0") 
+	(cxml:attribute "stop" (format nil "~f" last-time))
+	(cxml:with-element "markerDS"
+	  (loop
+	     for time across (val clap-times-in-seconds)
+	     for beat-index = 0 then (1+ beat-index)
+	     do (cxml:with-element "marker"
+		  (cxml:attribute "id" (format nil "~a" beat-index))
+		  (cxml:attribute "num" (format nil "~a" (1+ (mod beat-index meter))))
+		  (cxml:text (format nil "~f" time)))))))))
+
+(defun write-ircam-beat-marker-document (filepath media-description clap-times-in-seconds last-time)
+  (with-open-file (out filepath :direction :output :element-type '(unsigned-byte 8))
+    (write-ircam-beat-marker-stream out media-description clap-times-in-seconds 4 last-time)))
+
+;;(write-ircam-beat-marker-document #P"/Volumes/iDisk/Research/Data/IRCAM-Beat/test.xml"
+;; "testfile.wav" (make-narray '(1.0d0 2.0d0 2.5d0 3.0d0)) 9.0d0)
