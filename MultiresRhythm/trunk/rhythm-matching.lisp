9ec4d0b2b5b65191ee631fb0396201ef3b632d9e
leigh <leigh@fe1d2b8c-0a59-44fb-bae1-36f596c3a4c4>
2008-11-28 14:48:20 +0000

    Version which now produces proper matches between ODF and metrical structures, no longer requires the syncopation package.
    git-svn-id: file:///Users/leigh/Sources/SVN/Research_repos/Research@767 fe1d2b8c-0a59-44fb-bae1-36f596c3a4c4

--- MultiresRhythm/trunk/rhythm-matching.lisp
+++ MultiresRhythm/trunk/rhythm-matching.lisp
@@ -11,14 +11,29 @@
 ;;;; Copyright (c) 2008
 ;;;;
 
+(declaim (optimize (speed 0) (safety 3) (debug 3)))
+
 (in-package :multires-rhythm)
 (use-package :nlisp)
+
+;;; calculating metric salience the Longuet-Higgins & Lee way.
+(defun lh-metric-salience (meter)
+  "Return list of metric weights; 0 highest level, -n lowest"
+  (loop with grid-length = (apply #'* meter)
+        with salience = (make-list grid-length :initial-element nil)
+        initially (setf (elt salience 0) 0)
+        for division in meter
+        as level = -1 then (1- level)
+        as interval = (/ grid-length division) then (/ interval division)
+        do (loop for pos from 0 below grid-length by interval
+                 do (unless (elt salience pos) (setf (elt salience pos) level)))
+        finally (return salience)))
 
 ;;; Just needs the syncopation package loaded.
 (defun metric-scale (meter &key (max-amp 1.0d0) (min-amp 0.12d0))
   "Derive from Longuet-Higgin's & Lee's metric salience measure (which is inverted in
    polarity) an amplitude weighting"
-  (let* ((lhl-metric-salience (make-narray (syncopation::lh-metric-salience meter)))
+  (let* ((lhl-metric-salience (make-narray (lh-metric-salience meter)))
 	 (max-salience (.max lhl-metric-salience))
 	 (min-salience (.min lhl-metric-salience))
 	 (amp-scaling (/ (- max-salience min-salience) (- max-amp min-amp))))
@@ -27,14 +42,20 @@
 
 ;;; TODO this assumes a constant tempo. This needs replacing with a function that computes
 ;;; the metrical structure according to the tempo estimate at each beat.
-(defun metrically-scaled-rhythm (meter measures tempo)
+;;; Assumes 4 tatums make a beat (for bpm).
+(defun metrically-scaled-rhythm (meter measures tempo &key (sample-rate 200.0d0) (beats-per-measure 4))
   "Returns a rhythm with weighted onsets matching the metrical structure"
   (let* ((metrical-weights (metric-scale meter))
-	 ;; (number-of-tatums (* (1+ (reduce #'* meter)) measures))
-	 (number-of-tatums (* (.length metrical-weights) measures))
-	 (tatum-duration (/ 60.0d0 tempo 4.0)) ; in seconds, assumes 4 tatums make a beat (for bpm).
-	 (times (nlisp::array-to-list (.* tatum-duration (.iseq 0 number-of-tatums)))))
-    (rhythm-of-weighted-onsets "metrical scaling" (mapcar #'list times (nlisp::array-to-list metrical-weights)))))
+	 (number-of-tatums (1+ (* (.length metrical-weights) measures))) ; +1 for the next downbeat
+	 (tatums-per-beat (/ (reduce #'* meter) beats-per-measure))
+	 (tatum-duration (/ 60.0d0 tempo tatums-per-beat)) ; in seconds
+	 (times (nlisp::array-to-list (.* tatum-duration (.iseq 0 number-of-tatums))))
+	 ;; Append the number of measures worth of weights and a final full amplitude downbeat.
+	 (weights (append (loop repeat measures append (nlisp::array-to-list metrical-weights)) '(1.0d0))))
+    (rhythm-of-weighted-onsets "metrical scaling" 
+			       (mapcar #'list times weights)
+			       ;; :duration (* tatum-duration number-of-tatums)
+			       :sample-rate sample-rate)))
 
 (defun convolve (sig1 sig2)
   "Do a Fourier domain based convolution of the two signals, which may differ in length and be non-dyadic."
@@ -53,10 +74,8 @@
 	 (x-fft (fft (.* (end-pad x sufficient-pad-length) #C(1d0 0d0))))
 	 (y-conj-fft (.conjugate (fft (.* (end-pad y sufficient-pad-length) #C(1d0 0d0)))))
 	 (result (.realpart (ifft (.* x-fft y-conj-fft)))))
-    (.subseq (rotate-vector result :by (- (.length y))) 0 result-length)))
-
     ;; Divide by the length of the vector to renormalize the result.
-    ;; (./ result result-length)))
+    (./ (.subseq (rotate-vector result :by (- (.length y))) 0 result-length) result-length)))
 
 ;;; TODO should make the expectation precision value narrow or widen the Gaussian peak.
 (defun temporal-likelihood-of-expectations (expectations temporal-discrimination)
@@ -72,42 +91,72 @@
   (let* ((envelope-width (round (* temporal-discrimination (sample-rate rhythm))))
 	 (convolved-impulses (convolve (time-signal rhythm) (gaussian-envelope envelope-width)))
 	 ;; Rotate so that the Gaussian mean is centered on each impulse.
-	 (rotated-envelope (rotate-vector convolved-impulses :by (/ envelope-width 2))))
+	 (rotated-envelope (rotate-vector convolved-impulses :by (round envelope-width 2))))
     ;; Since we are interested in the convolved rhythm envelope alone, we remove the
     ;; trailing extra duration allowed for by the gaussian envelope (after rotation).
     (.subseq rotated-envelope 0 (duration-in-samples rhythm))))
 
-(defun gaussian-rhythm-envelopes (rhythms)
+(defun gaussian-rhythm-envelopes (rhythms &key (temporal-discrimination 0.60d0))
   "Given a list of rhythms, return a list of rhythm envelopes"
-  (mapcar #'gaussian-rhythm-envelope rhythms))
+  (mapcar (lambda (x) (gaussian-rhythm-envelope x :temporal-discrimination temporal-discrimination)) rhythms))
 
-(defun cross-correlation-match (meter measure-duration onset-detection-function)
-  (let* ((metric-accent-gaussian (gaussian-rhythm-envelope (metrically-scaled-rhythm meter measure-duration)))
-	 (acceptable-correlation-measures 1)
-	 (metric-ODF-correlation (cross-correlation onset-detection-function metric-accent-gaussian))
+;; Find the peaks in the positive lag region of the cross-correlation.  We only use
+;; positive lag regions since positive lag values effectively indicate how far to
+;; shift the ODF candidate *backwards* in time, thereby determining the meter
+;; play region start time. Negative lag values indicate how far to shift the
+;; candidate *forward* in time, but since we are nowdays only using phrases
+;; within a theme, this means playing before the start of the candidate media,
+;; meaning leading black or silence which would produce poor results (even if the
+;; correlation score was high).
+(defun cross-correlation-match (onset-detection-function metric-accent-gaussian &key (highest-correlations 10))
+  "Return the sample that is the highest match between the two vectors"
+  (let* ((metric-ODF-correlation (cross-correlation onset-detection-function metric-accent-gaussian))
 	 ;; Since the crosscorrelation rotates the result forward by the metric-accent-gaussian
 	 ;; length, the zeroth lag is at that element.
 	 (zeroth-lag (.length metric-accent-gaussian))
-	 (positive-lag-range (list zeroth-lag (.length metric-ODF-correlation)))
-	 ;; Find the peaks in the positive lag region of the cross-correlation.  
-	 ;; Why do we only use positive lag regions? It took me a while to figure out and
-	 ;; recall what I did also...  Positive lag values effectively indicate how far to
-	 ;; shift the ODF candidate *backwards* in time, thereby determining the play region
-	 ;; start time. Negative lag values indicate how far to shift the candidate *forward*
-	 ;; in time, but since we are nowdays only using phrases within a theme, this means
-	 ;; playing before the start of the candidate media, meaning leading black or silence
-	 ;; (in AVM-Odf-Matcher's case) which would produce poor results (even if the
-	 ;; correlation score was high).
-	 (metric-ODF-correlation-peaks (extrema-points (.subseq metric-ODF-correlation positive-lag-range))))
+	 (positive-lags (.subseq metric-ODF-correlation zeroth-lag))
+	 ;; Find that number of highest correlation measures, returning the locations in a
+	 ;; vector, in descending correlation order.
+	 (selected-peaks (.subseq (highest-peaks positive-lags) 0 highest-correlations)))
+    (diag-plot 'cross-correlation
+      (let* ((peak-values (make-double-array (.length positive-lags))))
+	(setf (.arefs peak-values selected-peaks) (.arefs positive-lags selected-peaks)) 
+	(nplot (list positive-lags peak-values) nil 
+	       :title (format nil "Cross Correlation of ~a" "...")
+	       :styles '("lines" "points") :aspect-ratio 0.2)))
+    selected-peaks))
 
-    ;; Find that number of highest correlation measures, returning the locations in a
-    ;; vector, in descending correlation order.
-    (.subseq metric-ODF-correlation-peaks 0 acceptable-correlation-measures)))
+(defun plot-correlation-matching (rhythm-1 rhythm-2 shift-by)
+  "Visually verifies the cross-correlation finds the shift to align the two rhythms"
+  (nplot (list rhythm-1 (.concatenate (make-double-array shift-by) rhythm-2)) nil :aspect-ratio 0.2))
+
+;;; TODO we can probably skip the normalisation for cross-correlation-matching.
+(defun match-ODF-meter (meter tempo-bpm onset-detection-rhythm)
+  "Returns the sample that the meter (at the given tempo) matches the onset detection rhythm at."
+  (let* ((metric-accent-gaussian (.normalise (gaussian-rhythm-envelope
+					      (metrically-scaled-rhythm meter 1 tempo-bpm 
+									:sample-rate (sample-rate
+										      onset-detection-rhythm)))))
+	 (normalised-odf (.normalise (time-signal onset-detection-rhythm)))
+	 (shift-options (cross-correlation-match normalised-odf metric-accent-gaussian)))
+    (diag-plot 'odf-match
+      (plot-correlation-matching normalised-odf metric-accent-gaussian (.aref shift-options 0)))
+    shift-options))
+
+(defun test-correlation-matching ()
+  "Visually verifies the cross-correlation finds the shift to align the two rhythms"
+  (let* ((rhythm-1 (gaussian-rhythm-envelope (rhythm-of-onsets "rhythm1" (make-narray '(1.0 3.0 3.5 4.0 5.0 5.5 6.0))) 
+					     :temporal-discrimination 0.3d0))
+	 (rhythm-2 (gaussian-rhythm-envelope (rhythm-of-onsets "rhythm2" (make-narray '(1.5 2.0 2.5)))))
+	 (shift-options (cross-correlation-match rhythm-1 rhythm-2))
+	 (shift-by (.aref shift-options 0)))
+    (format t "shift by ~a from options ~a~%" shift-by shift-options)
+    (plot-correlation-matching rhythm-1 rhythm-2 shift-by)))
 
 (defun match-expectations-to-candidates (expectations candidate-rhythms)
-  "Returns the measure of autocorrelation matches of the expectations against the
+  "Returns the measure of cross correlation matches of the expectations against the
   candidate rhythms"
-  (let* ((temporal-discrimination (* 0.060 sample-rate)) ; 60mSec best temporal discrimination.
+  (let* ((temporal-discrimination 0.060d0) ; 60mSec best temporal discrimination.
 	 (match-prototype (temporal-likelihood-of-expectations expectations temporal-discrimination)))
     ;; TODO since the candidate rhythms will not be expected to change often, we can
     ;; precalculate the Gaussian envelopes for each.
