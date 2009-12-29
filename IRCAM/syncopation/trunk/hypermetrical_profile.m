f4725a8503ff6fd23ce9b07d84794f7703b5afdc
leighsmi <leighsmi@fe1d2b8c-0a59-44fb-bae1-36f596c3a4c4>
2009-12-29 14:32:43 +0000

    Corrected the incorrect reshaping in the last correction...sigh
    git-svn-id: file:///Users/leigh/Sources/SVN/Research_repos/Research@931 fe1d2b8c-0a59-44fb-bae1-36f596c3a4c4

--- IRCAM/syncopation/trunk/hypermetrical_profile.m
+++ IRCAM/syncopation/trunk/hypermetrical_profile.m
@@ -1,6 +1,6 @@
 function [ profile ] = hypermetrical_profile( silence_observations, hypermetrical_period )
 %hypermetrical_profile Compute the metrical profile over 4 measures.
-% Use fixed 4 bar measure hypermeter for pop/rock.
+% Use hypermetrical_period = 4 bars for common hypermeter for pop/rock.
 
 tatums_per_measure = size(silence_observations, 1);
 num_of_measures = size(silence_observations, 2);
@@ -8,9 +8,9 @@ num_of_measures = size(silence_observations, 2);
 % reshape into a hypermetrical period so we average across the whole period.
 shortened_silences = silence_observations(:, 1 : num_of_measures - mod(num_of_measures, hypermetrical_period));
 num_of_hypermeasures = floor(num_of_measures / hypermetrical_period);
-phrase_of_silence_obs = reshape(shortened_silences, num_of_hypermeasures, tatums_per_measure * hypermetrical_period);
+phrase_of_silence_obs = reshape(shortened_silences, tatums_per_measure * hypermetrical_period, num_of_hypermeasures);
 
-profile = 1 - (sum(phrase_of_silence_obs, 1) ./ num_of_hypermeasures);
+profile = 1 - (sum(phrase_of_silence_obs, 2) ./ num_of_hypermeasures)';
 
 end
 
