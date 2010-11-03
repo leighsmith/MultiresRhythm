b4f9494d6d5be150672385c609fad26456ce3652
leighsmi <leighsmi@fe1d2b8c-0a59-44fb-bae1-36f596c3a4c4>
2010-11-03 16:43:06 +0000

    Changed to Matlab friendly comments
    git-svn-id: file:///Users/leigh/Sources/SVN/Research_repos/Research@999 fe1d2b8c-0a59-44fb-bae1-36f596c3a4c4

--- PhD/MultiresRhythmOctave/dyadicLength.m
+++ PhD/MultiresRhythmOctave/dyadicLength.m
@@ -1,9 +1,9 @@
-### -*- Octave -*-
-###
-### $Id:$
-###
-### Returns the length of the signal padded to a dyadic (power of two) length.
-###
+%%% -*- Octave -*-
+%%%
+%%% $Id:$
+%%%
+%%% Returns the length of the signal padded to a dyadic (power of two) length.
+%%%
 function paddedLength = dyadicLength(signal_length)
   paddedLength = 2^ceil(log2(signal_length));
 endfunction
