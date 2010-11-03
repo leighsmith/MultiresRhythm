7a1c93385be1888541396e3b455f7dc211628c15
leighsmi <leighsmi@fe1d2b8c-0a59-44fb-bae1-36f596c3a4c4>
2010-11-03 16:43:06 +0000

    Changed to Matlab friendly comments
    git-svn-id: file:///Users/leigh/Sources/SVN/Research_repos/Research@999 fe1d2b8c-0a59-44fb-bae1-36f596c3a4c4

--- cwt.m
+++ cwt.m
@@ -1,12 +1,12 @@
-### -*- Octave -*-
-###
-### $Id:$
-###
-### Pads the signal to a dyadic length, then calls dyadic_cwt(),
-### trimming off the returned result.
-###
+%%% -*- Octave -*-
+%%%
+%%% $Id:$
+%%%
+%%% Pads the signal to a dyadic length, then calls dyadic_cwt(),
+%%% trimming off the returned result.
+%%%
 function [magnitude, phase] = cwt(signal, voicesPerOctave, maxWaveletPeriod)
-  ## The wavelet transform operates on 1xN vector
+  %% The wavelet transform operates on 1xN vector
   [pad_signal, trim] = dyadicPad(signal);
 
   [padMagnitude, padPhase] = dyadic_cwt(pad_signal, voicesPerOctave, maxWaveletPeriod);
