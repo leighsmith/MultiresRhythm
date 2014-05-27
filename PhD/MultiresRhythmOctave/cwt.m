a9afc2571a25b6c61f329478e9f7149bd7e6b457
Leigh Smith <leigh@leighsmith.com>
2014-05-27 18:20:35 -0400

    Overhauled for use with latest Octave

--- PhD/MultiresRhythmOctave/cwt.m
+++ PhD/MultiresRhythmOctave/cwt.m
@@ -1,12 +1,11 @@
-%%% -*- Octave -*-
-%%%
-%%% $Id:$
-%%%
-%%% Pads the signal to a dyadic length, then calls dyadic_cwt(),
-%%% trimming off the returned result.
-%%%
+%%% Calculates CWT on 1-D signal.
+%%% [magnitude, phase] = cwt(signal, voicesPerOCtave, maxWaveletPeriod)
+%%% The continuous wavelet transform operates on 1xN vector, producing
+%%% 2-D magnitude and phase values for a given resolution per octave to a maximum
+%%% wavelet period (must be dyadic).
 function [magnitude, phase] = cwt(signal, voicesPerOctave, maxWaveletPeriod)
-  %% The wavelet transform operates on 1xN vector
+  %% Pads the signal to a dyadic length, then calls dyadic_cwt(),
+  %% trimming off the returned result.
   [pad_signal, trim] = dyadicPad(signal);
 
   [padMagnitude, padPhase] = dyadic_cwt(pad_signal, voicesPerOctave, maxWaveletPeriod);
@@ -14,3 +13,4 @@ function [magnitude, phase] = cwt(signal, voicesPerOctave, maxWaveletPeriod)
   magnitude = padMagnitude(:, trim);
   phase = padPhase(:, trim);
 endfunction
+%%% -*- Octave -*-
